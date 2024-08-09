f376c8c53676eeb8c5be524143fb08c63a435c9a
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-10 01:55:15 +0900

    Title: 흰검 칠하기, Time: 292ms, Memory: 50MB, Status: Passed - CodeTree

--- 240810/흰검 칠하기/painting-white-black.py
+++ 240810/흰검 칠하기/painting-white-black.py
@@ -6,11 +6,13 @@ for i in range(n):
     a,b = input().split()
     plus = 1 if b == 'R'else -1
     for i in range(int(a)):
-        if not (color[idx][0] == 2 and color[idx][1] == 2):
+        if not (color[idx][0] >= 2 and color[idx][1] >= 2):
             li[idx] = 1 if b == 'R' else 2
             color[idx][b == 'R'] += 1
-        if color[idx][0] == 2 and color[idx][1] == 2:
+        
+        if color[idx][0] >= 2 and color[idx][1] >= 2:
             li[idx] = 3
+            
         if i == int(a)-1:break
         idx += plus
-print(li.count(3), li.count(1), li.count(2))
+print(li.count(2), li.count(1),li.count(3))
