473e4b4cf092235d85839f0845fc09a051292441
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-09 22:40:45 +0900

    Title: 홀수 짝수의 묶음, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240809/홀수 짝수의 묶음/odd-even-bundle.py
+++ 240809/홀수 짝수의 묶음/odd-even-bundle.py
@@ -14,13 +14,8 @@ if not hol:
     cnt = 1
 else:#짝수가 다 썼음 2,1,2,1
     # 1일 때 2라면
-    minus = 2
-    while hol:
-        hol -= minus
-        minus =2 if minus == 1 else 1
-        cnt += 1
-        if hol == 0:break
-        elif hol < 0:
-            cnt -= 2
-            break
+    result += hol // 3
+    hol %= 3
+    if hol == 1:cnt = -2
+    elif hol == 2:cnt = 1
 print(result * 2 + cnt)
