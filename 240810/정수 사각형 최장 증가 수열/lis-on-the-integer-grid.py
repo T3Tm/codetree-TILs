44242b7b87d26b3ff0b085d3264dfa77a7c9591d
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-10 19:43:16 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 513ms, Memory: 55MB, Status: Passed - CodeTree

--- 240810/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240810/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,27 +1,23 @@
-import sys
-sys.setrecursionlimit(250002)
-def dfs(x,y):
-    if dp[x][y]!=-1:
-        return dp[x][y]
-    mx = 0
-    for dx, dy in (0,1), (0,-1),(1,0),(-1,0):
-        nx, ny = x + dx, y + dy
-        if nx < 0 or nx >=n or ny < 0 or ny>=n:continue
-        if board[nx][ny] <= board[x][y] :continue
-        if dp[nx][ny]!=-1:
-            mx = max(mx,dp[nx][ny])
-            continue
-        mx = max(mx,dfs(nx,ny))
-    dp[x][y] = mx + 1
-    return dp[x][y]
-
 n = int(input())
 
-board = [[*map(int,input().split())]for _ in range(n)]
+board = [[0]*(n+1)] + [[0] + [*map(int,input().split())]for _ in range(n)]
 
-dp = [[-1] * n for _ in range(n)]
+dp = [[-1] * (n+1) for _ in range(n+1)]
+
+
+data = [(board[i][j],i,j) for i in range(1,n+1) for j in range(1,n+1)]
+
+data.sort(key = lambda x: (x[0]))
+
 result = -1
-for i in range(n):
-    for j in range(n):
-        result = max(result, dfs(i,j))
+for value, x, y in data:
+    dp[x][y] = max(dp[x][y], 1)
+    
+    for dx, dy in (0,1), (0,-1),(1,0),(-1,0):
+        nx, ny = x + dx, y +dy
+        
+        if nx < 1 or nx > n or ny < 1 or ny> n:continue
+        if board[nx][ny] <= value:continue
+        dp[nx][ny] = max(dp[nx][ny], dp[x][y] + 1)
+        result = max(result, dp[nx][ny])
 print(result)
