66adbfff9c68aa934915f214cb1b7c42c4ca6797
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-10 20:18:11 +0900

    Title: 최단 Run Length 인코딩, Time: 154ms, Memory: 25MB, Status: Passed - CodeTree

--- 240810/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 240810/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -5,13 +5,13 @@ cnt = 10 ** 8
 for i in range(len(word)):
     q.rotate(1)
     words = ''.join(q)
-    now = 0
     idx = 0
+    b = ''
     while idx < len(words):
         inner = idx
         while inner < len(words) and words[idx] == words[inner]:
             inner += 1
-        now += 2
+        b += f'{words[idx]}{inner-idx}'
         idx = inner
-    cnt = min(cnt,now)
+    cnt = min(cnt,len(b))
 print(cnt)
