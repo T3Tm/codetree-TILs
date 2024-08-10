f1ec0df4dc6997e5d4883ec13b2da0354b77f8d8
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-10 17:41:02 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 1303ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240810/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240810/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -3,12 +3,14 @@ sys.setrecursionlimit(250001)
 def dfs(x,y):
     if dp[x][y]!=-1:
         return dp[x][y]
-
     mx = 0
     for dx, dy in (0,1), (0,-1),(1,0),(-1,0):
         nx, ny = x + dx, y + dy
         if nx < 0 or nx >=n or ny < 0 or ny>=n:continue
         if board[nx][ny] <= board[x][y] :continue
+        if dp[nx][ny]!=-1:
+            mx = max(mx,dp[nx][ny])
+            continue
         mx = max(mx,dfs(nx,ny))
     dp[x][y] = mx + 1
     return dp[x][y]
