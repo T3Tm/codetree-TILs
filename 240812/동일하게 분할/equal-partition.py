9d70e77c3317950ddbade6a9b175314c30ae438d
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-12 00:34:03 +0900

    Title: 동일하게 분할, Time: 136ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240812/동일하게 분할/equal-partition.py
+++ 240812/동일하게 분할/equal-partition.py
@@ -1,6 +1,17 @@
 n = int(input())
-a = [*map(int,input().split())]
-if sum(a)/2 == sum(a) // 2:
+lis = [*map(int,input().split())]
+lis.sort()
+mid = sum(lis) // 2#이 수를 두 번만 만들면 됨.
+dp = [[0]*(10**5+1) for _ in range(2)]
+dp[0][0] = 1
+dp[1][0] = 1
+ptr = 0
+for num in lis:
+    for i in range(10**5,num-1,-1):
+        dp[ptr][i] += dp[ptr][i-num]
+    if dp[ptr][mid]:
+        ptr += 1
+if ptr==2:
     print('Yes')
 else:
     print('No')
