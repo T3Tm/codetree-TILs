cffb0525a8cbcc36c0aa7282b9df7b3f8ebbeadd
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-12 01:06:23 +0900

    Title: 동일하게 분할, Time: 612ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240812/동일하게 분할/equal-partition.py
+++ 240812/동일하게 분할/equal-partition.py
@@ -1,17 +1,24 @@
 n = int(input())
 lis = [*map(int,input().split())]
 lis.sort()
-mid = sum(lis) // 2#이 수를 두 번만 만들면 됨.
-dp = [[0]*(10**5+1) for _ in range(2)]
-dp[0][0] = 1
-dp[1][0] = 1
-ptr = 0
+mid = (total:=sum(lis)) // 2#이 수를 두 번만 만들면 됨.
+dp = [0]*(total+1)
+dp[0] = 1
+dp_num = [[]for _ in range(total+1)]
 for num in lis:
-    for i in range(10**5,num-1,-1):
-        dp[ptr][i] += dp[ptr][i-num]
-    if dp[ptr][mid]:
-        ptr += 1
-if ptr==2:
+    if dp[mid]:
+        dp_num[mid].sort()
+        for nums in dp_num[mid][::-1]:
+            for i in range(mid,nums-1,-1):
+                if nums in dp_num[i]:
+                    dp[i] = max(dp[i]-1,0)
+    for i in range(total,num-1,-1):
+        dp[i] += dp[i-num]
+        if dp[i]:
+            dp_num[i].extend(dp_num[i-num])
+            dp_num[i].append(num)
+    
+if dp[mid]: # 5 5 5 5 평균이 5
     print('Yes')
 else:
     print('No')
