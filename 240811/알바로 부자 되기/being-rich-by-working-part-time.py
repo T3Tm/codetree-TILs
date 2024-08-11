c0ff72d515e95e42b766772305c63b5ba4c7d49b
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-11 23:40:47 +0900

    Title: 알바로 부자 되기, Time: 163ms, Memory: 25MB, Status: Passed - CodeTree

--- 240811/알바로 부자 되기/being-rich-by-working-part-time.py
+++ 240811/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -1,11 +1,9 @@
-from bisect import bisect_left
 n = int(input())
-dp = [0]
-end_line = [0]
+dp = []
+end_line = []
     
 for i in range(n):
     s,e,p = map(int,input().split())
-    
     now = 0
     for j in range(i):
         if end_line[j] < s:
