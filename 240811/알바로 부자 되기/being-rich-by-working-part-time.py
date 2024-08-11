2962cd5b20e71e3f4297b6f34257bcb203e839b9
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-11 23:52:15 +0900

    Title: 알바로 부자 되기, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240811/알바로 부자 되기/being-rich-by-working-part-time.py
+++ 240811/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -1,13 +1,18 @@
+from bisect import bisect_left
 n = int(input())
 dp = []
 end_line = []
-    
-for i in range(n):
+#끝나는 시간 , value
+for _ in range(n):
     s,e,p = map(int,input().split())
-    now = 0
-    for j in range(i):
-        if end_line[j] < s:
-            now = max(now,dp[j])
-    dp.append(now + p)
-    end_line.append(e)
+    idx = bisect_left(end_line,s)
+    v = max(dp[:idx] + [0])
+    if idx == len(dp):
+        end_line.append(e)
+        dp.append(v+p) 
+    elif end_line[idx] == e:
+        dp[idx] = max(dp[idx], v + p)
+    else:
+        end_line.insert(idx,e)
+        dp.insert(idx,v + p)
 print(max(dp))
