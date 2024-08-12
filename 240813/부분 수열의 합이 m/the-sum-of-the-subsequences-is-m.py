218b6a8344268d5fa61370cf91a86cf563a87e39
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-13 01:26:33 +0900

    Title: 부분 수열의 합이 m, Time: 116ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240813/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.py
@@ -0,0 +1,15 @@
+n , m = map(int,input().split())
+li = [*map(int,input().split())]
+
+li.sort()
+
+dp = [10001] * (m+1)
+dp[0] = 0
+
+
+
+for num in li:
+    for i in range(m,num-1,-1):
+        dp[i] = min(dp[i], dp[i-num]+1)
+result = dp[m]
+print([result, -1][result == 10001])
