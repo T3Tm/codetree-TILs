8cece9f224adde0b7aed694c760784007291e593
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-12 14:23:23 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 2534ms, Memory: 312MB, Status: Passed - CodeTree

---
+++ 240812/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(250002)
+def dfs(x,y):
+    if dp[x][y]!=-1:
+        return dp[x][y]
+    mx = 0
+    for dx, dy in (0,1), (0,-1),(1,0),(-1,0):
+        nx, ny = x + dx, y + dy
+        if nx < 0 or nx >=n or ny < 0 or ny>=n:continue
+        if board[nx][ny] <= board[x][y] :continue
+        if dp[nx][ny]!=-1:
+            mx = max(mx,dp[nx][ny])
+            continue
+        mx = max(mx,dfs(nx,ny))
+    dp[x][y] = mx + 1
+    return dp[x][y]
+
+n = int(input())
+
+board = [[*map(int,input().split())]for _ in range(n)]
+
+dp = [[-1] * n for _ in range(n)]
+result = -1
+for i in range(n):
+    for j in range(n):
+        result = max(result, dfs(i,j))
+print(result)
