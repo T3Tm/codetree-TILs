87c2f1a847c3d36f21758263cdd6ada21cf9452f
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-13 17:23:32 +0900

    Title: 막대기 나누기, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 240813/막대기 나누기/rod-cutting.py
+++ 240813/막대기 나누기/rod-cutting.py
@@ -4,7 +4,7 @@ li = [0] + [*map(int,input().split())]
 dp = [0] * (n+1)
 #dp[i] = i원일 때 최대 수익 저장
 for i in range(1,n+1):#i원일 때
-    for num in range(1,5):#num의 숫자들로
+    for num in range(1,n+1):#num의 숫자들로
         if num > i:break
         dp[i] = max(dp[i],dp[i-num] + li[num])
 print(dp[n])
