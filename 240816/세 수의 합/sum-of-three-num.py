403a71c6aaa7f4e33ac4eb2be425d65f935a68b4
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-16 00:02:57 +0900

    Title: 세 수의 합, Time: 312ms, Memory: 51MB, Status: Passed - CodeTree

---
+++ 240816/세 수의 합/sum-of-three-num.py
@@ -0,0 +1,19 @@
+n, k = map(int, input().split())
+
+dic = {}
+for i in range(2, n):
+    dic[i] = {}
+
+li = [*map(int,input().split())]
+
+cnt = 0
+
+for i in range(n-2):
+    for j in range(i+1, n-1):
+        dic[j+1][li[i] + li[j]] = dic[j+1].get(li[i] + li[j], 0) + 1
+
+
+for z in range(2, n):
+    for i in range(2,z+1):
+        cnt += dic[i].get(k - li[z], 0)
+print(cnt)
