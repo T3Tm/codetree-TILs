4ade02048d47fc0ec66007ea57bb04691e7ed0ba
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-16 21:23:49 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 316ms, Memory: 38MB, Status: Wrong Answer - CodeTree

--- 240816/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 240816/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -5,26 +5,29 @@ n = int(input())
 board = [[*map(int,input().split())]for _ in range(n)]
 
 visited = [[-1] * n for _ in range(n)]
-def dfs(x, y, gap, mini):
-    visited[x][y] = gap
+def dfs(x, y, gap, mini, vis):
+    visited[x][y] = vis
     if x == n-1 and y == n-1:
         return 1
     
     for dx, dy in (0,1), (0,-1),(1,0), (-1,0):
         nx ,ny = x + dx, y + dy
         if nx < 0 or nx >=n or ny < 0 or ny >=n:continue
-        if visited[nx][ny] == gap:continue
+        if visited[nx][ny] == vis:continue
         if board[nx][ny] < mini or board[nx][ny] > mini + gap:continue
-        ret = dfs(nx, ny, gap, mini)
+        ret = dfs(nx, ny, gap, mini, vis)
 
         if ret:return 1
     return 0
 flag = 0
+
+v = 0
 for gap in range(1, 101):
     for mini in range(1, 101):
         if board[0][0] < mini or board[0][0] > mini + gap:continue
-        if dfs(0,0, gap, mini):
+        if dfs(0,0, gap, mini, v):
             print(gap)
             flag = 1
             break
+        v+=1
     if flag:break
