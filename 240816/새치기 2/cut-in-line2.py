808dbae382fad1162e131833f0bec03962ab7085
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-16 17:51:05 +0900

    Title: 새치기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240816/새치기 2/cut-in-line2.py
+++ 240816/새치기 2/cut-in-line2.py
@@ -1,143 +1,166 @@
 from math import ceil
+from collections import deque
+import sys
+input = sys.stdin.readline
 
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-        self.prev = None
-
-class DoubleLinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-    def isEmpty(self):
-        return self.head == None
-    
-    def insertBegginning(self, newNode):
-        if self.head == None:
-            self.head = newNode
-            self.tail = newNode
-            return
-        self.insertBefore(self.head, newNode)
-    def insertBefore(self, prev, next):
-        next.next = prev
-        if prev.prev == None:#헤드 노드
-            self.head = next
-            prev.prev = next
-            return
-        prev.prev.next = next
-        next.prev = prev.prev
-        prev.prev = next
-    def insertEnd(self, newNode: Node):
-        if self.tail == None:
-            self.head = newNode
-            self.tail = newNode
-            return
-        self.insertAfter(self.tail, newNode)
-    def insertAfter(self, node: Node, newNode: Node):
-        newNode.prev = node
-        if node.next == None:
-            self.tail = newNode
-            node.next = newNode    
-            return
-        newNode.next = node.next
-        node.next.prev = newNode
-        node.next = newNode
-    def remove(self,node: Node):
-        if node.prev != None:
-            node.prev.next = node.next
-        else:
-            if node.next != None:
-                node.next.prev = None
-            self.head = node.next
-        
-        if node.next != None:
-            node.next.prev = node.prev
-        else:
-            if node.prev != None:
-                node.prev.next = None
-            self.tail = node.prev
-#n명의 사람들이 M개의 여러 줄에 나눠서 줄을 서고 있다.
-
-#1 a b a인 사람이 b사람 새치기했음.
-#2 a a가 집으로 갔음
-#3 이름이 a부터 b인 사람이 통째로 c인 사람 앞으로 새치기 했음
 n, m, q = map(int,input().split())
 
-x = n // m # 2
+li = [*input().split()]
 
-li = input().split()
-cache = [[' ']*(x+1) for _ in range(m+1)]
-result = [DoubleLinkedList() for _ in range(m+1)]
+x = n//m #2이다.
+
+cache = [deque() for _ in range(m)]
 for i in range(1,n+1):
-    value = li[i-1]
-    row = ceil(i/x)
-    cache[row][(i-1)%x + 1] = value
+    row = ceil(i / x) - 1
+    column = (i-1)%x 
+    cache[row].append(li[i-1])
 
-for i in range(1,m+1):
-    for j in range(1,x+1):
-        if cache[i][j] == ' ':break
-        node = Node(cache[i][j])
-        result[i].insertEnd(node)
-
-#a인 사람 찾기
-
+def find(name):
+    for row in range(m):
+        for column in range(len(cache[row])):
+            if cache[row][column] == name:#찾았음
+                return [row, column]
 for _ in range(q):
-    cmd, n1, *n2 = input().split()
+    cmd, a, *b = input().split()
     if cmd == '1':
-        n3 = ''
-        n4 = ''
-        for i in range(1, m+1):
-            node = result[i].head
-
-            while node:
-                if node.value == n1:
-                    result[i].remove(node)
-                    n3 = node
-                elif node.value == n2[0]:
-                    #이 노드에다가 넣어야 됨.
-                    n4 = (result[i], node)
-                    break
-                node = node.next
-        n4[0].insertBefore(n4[1], n3)
-    elif cmd == '2':#이 사람은 집을 갔다는 것은 노드에서 빼야됨.
-        for i in range(1,m+1):#행을 돌면서 해당 숫자 찾기
-            node = result[i].head
-            
-            while node and node.value != n1:
-                node = node.next
-            if node and node.value == n1:#찾았음
-                result[i].remove(node)#이 노드 삭제하기
-                break
-    else:
-        n3 = ''
-        n4 = ''
-        temp = []
-        for i in range(1, m + 1):
-            node = result[i].head
-
-            while node:
-                if node.value == n1:
-                    while node and node.value != n2[0]:
-                        n1 = node.next
-                        temp.append(node.value)#넣어야할 이름들 적어놓기
-                        result[i].remove(node)
-                        node = n1
-                    result[i].remove(node)
-                    break
-                elif node.value == n2[1]:
-                    n4 = (result[i], node)
-                    break
-        for name in temp[::-1]:
-            node = Node(name)
-            n4[0].insertBefore(n4[1], node)
-            n4[1] = node
-            
+        #a와 b의 행과 열 찾아주기
+        a_result = find(a)
+        #a를 꺼낼 것인데 column이 뒤가 가까운지 앞이 가까운지 체크
+        if len(cache[a_result[0]]) >> 1 > a_result[1]:
+            #앞이 더 가깝기 때문에 a_result[1]까지 다 빼내기
+            temp = []
+            for _ in range(a_result[1]-1):
+                temp.append(cache[a_result[0]].popleft())
+            cache[a_result[0]].popleft()#자기 자신까지 나오기
+            while temp:
+                name = temp.pop()
+                cache[a_result[0]].appendleft(name)
+        else:
+            temp = []
+            for _ in range(len(cache[a_result[0]])-a_result[1]-1):
+                temp.append(cache[a_result[0]].pop())
+            cache[a_result[0]].pop()#자기 자신까지 나오기
+            while temp:
+                name = temp.pop()
+                cache[a_result[0]].append(name)
         
+        b_result = find(b[0])
+        if len(cache[b_result[0]]) >> 1 > b_result[1]:
+            #앞으로 빼는 것이 더 빠름
+            temp = []
+            for _ in range(b_result[1]-1):
+                temp.append(cache[b_result[0]].popleft())
+            cache[b_result[0]].appendleft(a)#a를 넣기
+            while temp:#다시 원복
+                name = temp.pop()
+                cache[b_result[0]].appendleft(name)
+        else:
+            temp = []
+            for _ in range(len(cache[b_result[0]])-b_result[1]):
+                temp.append(cache[b_result[0]].pop())
+            cache[b_result[0]].append(a)#a를 넣기
+            while temp:#다시 원복
+                name = temp.pop()
+                cache[b_result[0]].append(name)
+    elif cmd == '2':
+        a_result = find(a)
+        if len(cache[a_result[0]]) >> 1 > a_result[1]:
+            #앞이 더 가깝기 때문에 a_result[1]까지 다 빼내기
+            temp = []
+            for _ in range(a_result[1]-1):
+                temp.append(cache[a_result[0]].popleft())
+            cache[a_result[0]].popleft()#자기 자신까지 나오기
+            while temp:
+                name = temp.pop()
+                cache[a_result[0]].appendleft(name)
+        else:
+            temp = []
+            for _ in range(len(cache[a_result[0]])-a_result[1]-1):
+                temp.append(cache[a_result[0]].pop())
+            cache[a_result[0]].pop()#자기 자신까지 나오기
+            while temp:
+                name = temp.pop()
+                cache[a_result[0]].append(name)
+                
+    else:#a에서 b인원 통째로 c 앞으로 새치기
+        a_result = find(a)
+        b_result = find(b[0])
+        
+        if a_result[1] + 1 > len(cache[b_result[0]]) - b_result[1]:
+            #뒤로 빼는 것이 무조건 이득
+            temp = []
+            while cache[b_result[0]] and cache[b_result[0]][-1] != b[0]:
+                temp.append(cache[b_result[0]].pop())
+            
+            save = []
 
-for i in range(1,m+1):
-    head = result[i].head
-    while head != None:
-        print(head.value, end=' ')
-        head = head.next
-    print()
+            while cache[a_result[0]] and cache[a_result[0]][-1] != a:
+                save.append(cache[a_result[0]].pop())
+            
+            if cache[a_result[0]]:
+                save.append(cache[a_result[0]].pop())#자기 자신까지 저장
+            #temp에 있는 것은 다시 넣어놓아야 함.
+            while temp:cache[a_result[0]].append(temp.pop())
+            
+            
+            c_result = find(b[1])
+            if len(cache[c_result[0]]) >> 1 > c_result[1]:#앞으로 빼서 넣는 것이 더 이득
+                tt = []
+                while cache[c_result[0]] and cache[c_result[0]][0] != b[1]:
+                    tt.append(cache[c_result[0]].popleft())
+
+                while save:
+                    cache[c_result[0]].appendleft(save.pop())
+            else:
+                tt = []
+                while cache[c_result[0]] and cache[c_result[0]][-1] != b[1]:
+                    tt.append(cache[c_result[0]].pop())
+                
+                l = cache[c_result[0]].pop()#자기 자신 빼야됨.
+                                
+                while save:
+                    cache[c_result[0]].append(save.pop())
+                
+
+                cache[c_result[0]].append(l)
+                
+        else:
+            #앞으로 빼야됨.
+            temp = []
+            while cache[a_result[0]] and cache[a_result[0]][0] != a:
+                temp.append(cache[a_result[0]].popleft())
+            
+            save = []
+
+            while cache[b_result[0]] and cache[b_result[0]][0] != b[0]:
+                save.append(cache[b_result[0]].popleft())
+            if cache[b_result[0]]:
+                save.append(cache[b_result[0]].popleft())#b 자기 자신까지 저장
+            #temp에 있는 것은 다시 넣어놓아야 함.
+            while temp:cache[b_result[0]].appendleft(temp.pop())
+            
+            c_result = find(b[1])
+            if len(cache[c_result[0]]) >> 1 > c_result[1]:#앞으로 빼서 넣는 것이 더 이득
+                tt = []
+                while cache[c_result[0]] and cache[c_result[0]][0] != b[1]:
+                    tt.append(cache[c_result[0]].popleft())
+
+                while save:
+                    cache[c_result[0]].appendleft(save.pop())
+            else:
+                tt = []
+                while cache[c_result[0]] and cache[c_result[0]][-1] != b[1]:
+                    tt.append(cache[c_result[0]].pop())
+                
+                l = cache[c_result[0]].pop()
+                
+                while save:
+                    cache[c_result[0]].append(save.pop())
+                
+                cache[c_result[0]].append(l)
+for i in range(m):
+    if cache[i]:
+        print(*cache[i])
+    else:
+        print(-1)
+#5만
