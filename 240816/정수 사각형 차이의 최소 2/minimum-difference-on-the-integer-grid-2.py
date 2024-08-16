bf2c45336b2ca92f2e516d343f8eb2f6930da7c2
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-16 22:05:56 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 1550ms, Memory: 36MB, Status: Passed - CodeTree

--- 240816/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 240816/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -10,19 +10,19 @@ def dfs(x, y, gap, mini, vis):
     if x == n-1 and y == n-1:
         return 1
     
-    for dx, dy in (0,1), (0,-1),(1,0), (-1,0):
+    for dx, dy in (0,1),(1,0):
         nx ,ny = x + dx, y + dy
         if nx < 0 or nx >=n or ny < 0 or ny >=n:continue
         if visited[nx][ny] == vis:continue
         if board[nx][ny] < mini or board[nx][ny] > mini + gap:continue
         ret = dfs(nx, ny, gap, mini, vis)
-
         if ret:return 1
     return 0
 flag = 0
-
+#최소 50
+#갭이 50
 v = 0
-for gap in range(1, 101):
+for gap in range(100):
     for mini in range(1, 101):
         if board[0][0] < mini or board[0][0] > mini + gap:continue
         if dfs(0,0, gap, mini, v):
