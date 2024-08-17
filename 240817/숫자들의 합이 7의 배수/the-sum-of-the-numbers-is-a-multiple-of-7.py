f74ade54b30d2e48ec3d5016dec6007a432d63d0
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-17 23:10:34 +0900

    Title: 숫자들의 합이 7의 배수, Time: 5043ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

--- 240817/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.py
+++ 240817/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.py
@@ -6,15 +6,16 @@ prefix = [0]*(n+1)
 result = 0
 
 dic = {0:0}
-for i in range(1,n+1):
+for i in range(1,n+1):#n번
     prefix[i] = prefix[i-1] + li[i]
     #prefix와 가장 가까운 7의 배수 찾기?
     #각각 prefix에서 7의 배수 들을 찾기
     temp = prefix[i]
     mul = 1
-    while temp - mul * 7 >= 0:
-        now_seven = mul * 7 #temp에서 가장 가까운 7의 배수 이 값이 (temp - div*7) 이 값이 dic에 있으면 result 
-        result = max(result, i - dic.get(temp - now_seven, i))
+    while temp - mul * 7 >= 0:#13번 돈다.
+        now_seven = mul * 7 #temp에서 가장 가까운 7의 배수 이 값이 (temp - div*7) 이 값이 dic에 있으면 result
+        if temp - now_seven in dic:
+            result = max(result, i - dic[temp-now_seven])
         mul += 1
     dic[prefix[i]] = i#인덱스 지정
 print(result)
