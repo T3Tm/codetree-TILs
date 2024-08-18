4ae8880a8b7c09f1b6e42fb87872a802407dccd8
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-18 20:07:29 +0900

    Title: 최대 구간의 크기, Time: 703ms, Memory: 61MB, Status: Passed - CodeTree

---
+++ 240818/최대 구간의 크기/size-of-largest-interval.py
@@ -0,0 +1,32 @@
+import sys 
+input = sys.stdin.readline
+
+
+n = int(input())
+li = [[*map(int,input().split())]for _ in range(n)]
+
+li.sort()
+
+dic = {}
+
+lis = set()
+for (s,e) in li:
+    dic[s] = dic.get(s, 0) + 1
+    dic[e] = dic.get(e, 0) - 1
+    lis.add(s)
+    lis.add(e)
+
+lis = sorted(lis)
+
+first = lis[0]
+result = 0
+for idx in range(1, len(lis)):
+    s,e = lis[idx-1],lis[idx]
+    
+    dic[e] += dic[s]
+    if not dic[e]:
+        result = max(result,e - first)
+    
+    if not dic[s]:
+        first = e
+print(result)
