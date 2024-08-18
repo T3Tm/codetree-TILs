ec196b0a19592904ac281de664357c0e7d01cff0
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-18 20:33:38 +0900

    Title: 컴퓨터 이용시간, Time: 1261ms, Memory: 53MB, Status: Passed - CodeTree

--- 240818/컴퓨터 이용시간/computer-hours.py
+++ 240818/컴퓨터 이용시간/computer-hours.py
@@ -1,3 +1,4 @@
+from sortedcontainers import SortedSet
 import sys
 from heapq import*
 input = sys.stdin.readline
@@ -6,16 +7,17 @@ n = int(input())
 
 li = [[*map(int,input().split())] + [i] for i in range(n)]
 
-li.sort(key = lambda x: x[0])
+li.sort(key = lambda x:x[0])
 
 computer = []
+computer_num = SortedSet([i for i in range(1,n+1)])
 
 result = [0]*n
 for (s, e, idx) in li:
-    num = len(computer) + 1
-    while computer and computer[0][0] < s:#
+    while computer and computer[0][0] <= s:#
         pree, preidx = heappop(computer)
-        num = min(num, preidx)
-    result[idx] = num#
-    heappush(computer, (e, num))
+        computer_num.add(preidx)
+    result[idx] = computer_num[0]#제일 작은 숫자
+    heappush(computer, (e, computer_num[0]))
+    computer_num.discard(computer_num[0])
 print(*result)
