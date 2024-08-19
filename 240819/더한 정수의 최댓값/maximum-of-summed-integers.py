b82a8547286373dec1259f2abf45854a2c3516bc
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-19 21:00:12 +0900

    Title: 더한 정수의 최댓값, Time: 112ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 240819/더한 정수의 최댓값/maximum-of-summed-integers.py
+++ 240819/더한 정수의 최댓값/maximum-of-summed-integers.py
@@ -1,6 +1,7 @@
-n = int(input())
-li = [*map(int,input().split())]
-if li[0]+li[1] < li[-1] + li[-2]:
-    print(2*sum(li[2:]))
-else:
-    print(2*sum(li[:-2]))
+n =int(input())
+li=[*map(int,input().split())]
+v=sum(li[2:])*2
+vv=sum(li[:-2])*2
+index = li[1:].index(min(li[1:-1]))+1
+vvv=sum(li[1:index+1])+sum(li[index:-1])
+print(max(v,vv,vvv))
