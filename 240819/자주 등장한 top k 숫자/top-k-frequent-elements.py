be8f1c75d344661f88c87b3f8640331b560d70ee
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-19 23:41:20 +0900

    Title: 자주 등장한 top k 숫자, Time: 369ms, Memory: 73MB, Status: Passed - CodeTree

--- 240819/자주 등장한 top k 숫자/top-k-frequent-elements.py
+++ 240819/자주 등장한 top k 숫자/top-k-frequent-elements.py
@@ -4,5 +4,5 @@ dic = {}
 for i in map(int,input().split()):
     dic[i] = dic.get(i, 0) + 1
 
-
-print(*map(lambda x: x[0],sorted(filter(lambda x: x[1]>=k, dic.items()), key = lambda x: (x[1], -x[0]))))
+filter_sort = sorted(dic.items(), key = lambda x: (-x[1], -x[0]))
+print(*[*map(lambda x: x[0], filter_sort)][:k])
