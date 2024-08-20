764cb426ca19457a2e9b66b1d8844c06e34f8854
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-20 13:59:06 +0900

    Title: 1이 k개 이상 존재하는 부분 수열, Time: 295ms, Memory: 97MB, Status: Wrong Answer - CodeTree

---
+++ 240820/1이 k개 이상 존재하는 부분 수열/subsequence-with-k-or-more-1's.py
@@ -0,0 +1,17 @@
+n, k =map(int,input().split())
+li = [*map(int,input().split())]
+
+s = 0
+left = 0
+right = 0
+result = 1000001
+while right <= n:
+    if s < k:
+        if right == n:break
+        s += li[right] == 1
+        right += 1
+    else:#s >= k 되는 순간
+        result = min(result,(right-1) - (left - 1))
+        s -= li[left] == 1
+        left += 1
+print(result)
