e09769905fbe9d14704cf9f7003ab05c19974999
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-22 16:51:51 +0900

    Title: 연결된 정점 2, Time: 352ms, Memory: 29MB, Status: Passed - CodeTree

--- 240822/연결된 정점 2/connected-vertex-2.py
+++ 240822/연결된 정점 2/connected-vertex-2.py
@@ -1,16 +1,25 @@
 import sys
-sys.setrecursionlimit(100002)
 input = sys.stdin.readline
 
 SIZE = 100001
 def find(x):
     if parent[x] < 0 :return x
-    parent[x] = find(parent[x])
-    return parent[x]
+    #부모 노드
+    cur = x
+    while parent[cur] >= 0:
+        cur = parent[cur]
+    
+    while parent[x] >= 0:
+        next = parent[x]
+        parent[x] = cur
+        x = next#부모 따라가기, 해당 곳에 cur로 좌압
+    
+    return cur
 
 def merge(x, y):
     x = find(x)
     y = find(y)
+    if x == y:return
     if y > x: x,y = y,x
     parent[y] += parent[x]#이 정점의 부모 차수
     node_cnt[y] += node_cnt[x]#부모의 갯수
