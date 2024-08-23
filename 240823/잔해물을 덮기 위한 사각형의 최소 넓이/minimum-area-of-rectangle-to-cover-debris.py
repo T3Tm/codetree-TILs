401649cdfdf4b3e67828061bd442ead28d3fb882
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-23 15:49:54 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240823/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240823/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -1,15 +1,15 @@
-x1,y1,x2,y2 = map(lambda x: int(x) + 1000, input().split())
+x1,y1,x2,y2 = map(lambda x: int(x) + 100, input().split())
 
-x3,y3,x4,y4 = map(lambda x: int(x) + 1000, input().split())
+x3,y3,x4,y4 = map(lambda x: int(x) + 100, input().split())
 
-board = [[0]*2002 for _ in range(2002)]
+board = [[0]*202 for _ in range(202)]
 
 for i in range(x1, x2+1):
     for j in range(y1, y2+1):
         board[i][j] = 1
 
-for i in range(x3,x4 +1):
-    for j in range(y3, y4+1):
+for i in range(x3,x4 ):
+    for j in range(y3, y4):
         board[i][j] = 0
 
 minix = 2003
@@ -28,4 +28,4 @@ for i in range(x1, x2+1):
             maxiy = max(maxiy, j)
 if minix == 2003:print(0)
 else:
-    print( (maxix - minix)*(maxiy - miniy))
+    print((maxix - minix)*(maxiy - miniy))
