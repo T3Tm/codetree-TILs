2657428077ea16ff889c1f308963a2b375c7264b
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-24 23:01:19 +0900

    Title: 뱀은 사과를 좋아해, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240824/뱀은 사과를 좋아해/snake-loves-apples.py
@@ -0,0 +1,51 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n, m, k = map(int , input().split())
+
+
+arr = [[0]*(n+1) for _ in range(n+1)]
+for _ in range(m):
+    a, b = map(int ,input().split())
+    arr[a][b] = 1
+
+
+q = deque([[1,1]])
+dirs = {
+    'U' : 0,
+    'D' : 2,
+    'R' : 1,
+    'L' : 3
+}
+
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+
+
+time = 0
+for _ in range(k):
+    dir, cnt = input().split()
+    cnt = int(cnt)
+
+    for i in range(cnt):
+        x, y = q[0]
+        nx,ny = x + dx[dirs[dir]], y + dy[dirs[dir]]
+        time += 1
+        if nx < 1 or nx > n or ny < 1 or ny > n:#종료 돼야함
+            print(time)
+            exit(0)
+        
+        #몸 박 확인
+        for i in range(1, len(q)):
+            if [nx, ny] == q[i]:#몸 박
+                print(time)
+                exit(0)
+        
+        if arr[nx][ny]:#해당 곳에 먹이가 있음
+            arr[nx][ny] = 0
+        else:#꼬리 줄이기
+            q.pop()
+        q.appendleft([nx, ny])
+        
+print(time)
