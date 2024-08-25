0a09b97e500387543307025bda9fb538ddbfc514
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-25 20:21:13 +0900

    Title: 서로 다른 부분 문자열의 수, Time: 5794ms, Memory: 27MB, Status: Passed - CodeTree

--- 240825/서로 다른 부분 문자열의 수/distinct-substrings-count.py
+++ 240825/서로 다른 부분 문자열의 수/distinct-substrings-count.py
@@ -2,8 +2,6 @@ word = input()
 
 s = len(word)
 
-save = set()
-
 p = [31, 37]
 m = [10**9 + 7, 10 ** 9 + 9]
 
@@ -16,9 +14,10 @@ for k in range(2):
 
 def to_int(ch):
     return ord(ch) - ord('a') + 1
-
+result = 0
 for cnt in range(1,s+1):#몇 개씩 자를 것인지
     my_hash = [0, 0]
+    save = set()
     for i in range(cnt):
         for k in range(2):
             my_hash[k] = (my_hash[k] + p_pow[k][cnt - i - 1] * to_int(word[i]))%m[k]
@@ -30,4 +29,5 @@ for cnt in range(1,s+1):#몇 개씩 자를 것인지
             my_hash[k] = ((my_hash[k] - to_int(word[i-1]) * p_pow[k][cnt - 1])*p[k] + to_int(word[i + cnt - 1]) + m[k]) % m[k]
         save.add(tuple(my_hash))
 
-print(len(save))
+    result += len(save)
+print(result)
