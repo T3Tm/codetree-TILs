7f38c9edd285e32657511ca11ec63f84c7f145ee
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-25 22:16:51 +0900

    Title: 문자열 일치 횟수, Time: 5030ms, Memory: 26MB, Status: Time Limit Exceed - CodeTree

--- 240825/문자열 일치 횟수/string-match-count.py
+++ 240825/문자열 일치 횟수/string-match-count.py
@@ -1,11 +1,9 @@
 n = int(input())
-#p에서 n번 오른쪽으로 한칸씩 shift 하며, T와 일치하는 횟수를 구하자.
-t = input()
-t_len = len(t)
-t = '#' + t * n
+t = '#' + input()
 p = '#' + input()
 
 p_len = len(p)
+t_len = len(t)
 
 f = [0] * p_len
 f[0] = -1
@@ -17,15 +15,20 @@ for i in range(1, p_len):
         j = f[j]
     
     f[i] = j + 1
-
-cnt = j = 0
-
-for i in range(1, t_len + n):
-    while j>=0 and p[j+1] != t[i]:
-        j = f[j]
-    
-    j += 1
-    if j == p_len - 1:
-        j = f[j]
-        cnt += 1
+cnt = 0
+for start in range(1,n+1):
+    j = 0
+    point = start % p_len
+    if not point:point = 1
+    for i in range(1, t_len):
+        while j>=0 and p[j+1] != t[point]:
+            j = f[j]
+        
+        j += 1
+        if j == p_len - 1:
+            j = f[j]
+            cnt += 1
+            break
+        point = (point + 1)%(p_len)
+        if not point:point = 1
 print(cnt)
