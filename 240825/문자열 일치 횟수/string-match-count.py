e85b3526742509363e43ec717185c949be167831
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-25 22:46:31 +0900

    Title: 문자열 일치 횟수, Time: 110ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240825/문자열 일치 횟수/string-match-count.py
+++ 240825/문자열 일치 횟수/string-match-count.py
@@ -1,34 +1,28 @@
 n = int(input())
+
 t = '#' + input()
-p = '#' + input()
+p = '#' + input() * 2
 
-p_len = len(p)
-t_len = len(t)
+t_len = len(t) #100000
+p_len = len(p) #200000
 
-f = [0] * p_len
+
+
+f = [0] * t_len
 f[0] = -1
-
-for i in range(1, p_len):
+for i in range(1, t_len):
     j = f[i-1]
-    
-    while j>=0 and p[j+1] != p[i]:
+    while j >= 0 and t[j+1] != t[i]:
         j = f[j]
     
     f[i] = j + 1
-cnt = 0
-for start in range(1,n+1):
-    j = 0
-    point = start % p_len
-    if not point:point = 1
-    for i in range(1, t_len):
-        while j>=0 and p[j+1] != t[point]:
-            j = f[j]
-        
-        j += 1
-        if j == p_len - 1:
-            j = f[j]
-            cnt += 1
-            break
-        point = (point + 1)%(p_len)
-        if not point:point = 1
+
+j = cnt = 0
+for i in range(1, p_len-1):
+    while j>=0 and p[j+1]!=p[i]:
+        j = f[j]
+    j+= 1
+    if j == t_len - 1:
+        cnt += 1
+        j = f[j]
 print(cnt)
