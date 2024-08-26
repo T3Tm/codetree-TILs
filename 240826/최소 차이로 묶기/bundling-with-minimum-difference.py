103131effa1e499f6f79c43d3f93d39cfa0c1454
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-26 21:20:18 +0900

    Title: 최소 차이로 묶기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240826/최소 차이로 묶기/bundling-with-minimum-difference.py
+++ 240826/최소 차이로 묶기/bundling-with-minimum-difference.py
@@ -1,13 +1,27 @@
+INF = 10**9 + 1
 n, m = map(int, input().split())
 
-arrn = [*map(int,input().split())]
+arrn = [*map(int,input().split())] #무조건 n이 m보다 크거나 같게 만든다
 arrm = [*map(int,input().split())]
 
+if m > n:
+    n,m = m,n
+    tmp = arrn[::]
+    arrn = arrm[::]
+    arrm = tmp[::]#깊은 복사
 arrn.sort(reverse=1)
 arrm.sort(reverse=1)
 
-result = 0
-for i in range(min(n,m)):
-    result += abs(arrn[i] - arrm[i])
+dp = [[INF]*(n)for _ in range(m)]
 
-print(result)
+
+def dfs(depth, index):#해당 depth에 index를 골랐을 때 최대 값
+    if depth == m:
+        return 0
+    if index == n:return INF
+    if dp[depth][index]!=INF:return dp[depth][index]
+
+    for j in range(index, n):#depth랑 j랑 차이 계산 후 더해주기
+        dp[depth][index] = min(dp[depth][index], dfs(depth+1,j+1) + abs(arrm[depth] - arrn[j]))
+    return dp[depth][index]
+print(dfs(0,0))
