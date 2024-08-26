365c5dd459e5700c3c5224dd33516dd2cb4ee843
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-27 00:20:42 +0900

    Title: 문자열을 최대로 채워넣기, Time: 615ms, Memory: 34MB, Status: Passed - CodeTree

---
+++ 240827/문자열을 최대로 채워넣기/padding-a-string.py
@@ -0,0 +1,34 @@
+t, m = input().split()
+t = '#'+ t
+m = int(m)
+
+
+pattern = [*map(lambda x: '#'+x, input().split())]
+f=[[0]*10001 for _ in range(m)]
+
+for k in range(m):
+    f[k][0] = -1
+
+    for i in range(1, len(pattern[k])):
+        j = f[k][i-1]
+        while j >= 0 and pattern[k][j+1] != pattern[k][i]:
+            j = f[k][j]
+        
+        f[k][i] = j + 1
+#모든 곳에 실패함수 만들고
+
+dp = [0]*(len(t)+1)
+j_m = [0] * m
+for i in range(1, len(t)):
+
+    for k in range(m):
+        while j_m[k] >= 0 and pattern[k][j_m[k] + 1] != t[i]:
+            j_m[k] = f[k][j_m[k]]
+        j_m[k] += 1
+
+        if j_m[k] == len(pattern[k])-1:
+            j_m[k] = f[k][j_m[k]]
+
+            dp[i] = max(dp[i], dp[i - (len(pattern[k])-1)] + len(pattern[k])-1)
+    dp[i] = max(dp[i], dp[i-1])
+print(max(dp))
