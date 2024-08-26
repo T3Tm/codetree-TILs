5e3ba2d3032b2454f07f83e113dc1f2ca80d9236
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-26 23:33:47 +0900

    Title: 가장 짧은 패턴 문자열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240826/가장 짧은 패턴 문자열/shortest-pattern-string.py
@@ -0,0 +1,31 @@
+t = '#'+input() #어차피 제일 길어봤자 T임
+#t를 2배 해놓고
+#aabaabaab
+
+f = [0] * len(t)
+f[0] = -1
+for i in range(1, len(t)):
+    j = f[i-1]
+    while j>=0 and t[j+1] != t[i]:
+        j = f[j]
+    
+    f[i] = j + 1
+
+
+left,right = 1, len(t)-1
+
+while left<=right:#17번 
+    mid = (left+right) >> 1
+    pattern = '#' + t[1:mid+1]*(len(t)//mid)
+
+    j = 0
+    for i in range(1, len(t)):
+        while j >= 0 and t[j+1]!=pattern[i]:
+            j = f[j]
+        j += 1
+        if j == len(t) - 1:
+            right = mid - 1
+            break
+    else:
+        left = mid + 1
+print(left)
