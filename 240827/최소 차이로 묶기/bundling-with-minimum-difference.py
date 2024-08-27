e5c754b18086ed410e326951c8e3cdcaa424c8a5
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-27 23:04:25 +0900

    Title: 최소 차이로 묶기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240827/최소 차이로 묶기/bundling-with-minimum-difference.py
+++ 240827/최소 차이로 묶기/bundling-with-minimum-difference.py
@@ -1,27 +1,28 @@
-import sys
-sys.setrecursionlimit(2020)
-INF = 10**9 + 1
-def dfs(x, y):
-    global maxi
-    if y == m:return 0
-    if x == n:return INF
-    if dp[x][y]!=INF:return dp[x][y]
-    dp[x][y] = min(dfs(x+1, y), dfs(x+1,y+1) + abs(N[x]-M[y]))
-    return dp[x][y]
-
 n, m = map(int, input().split())
 
-N = [0] + [*map(int, input().split())]
-M = [0] + [*map(int, input().split())]
-assert m == (len(M) -1)
+arrn = [*map(int,input().split())] 
+arrm = [*map(int,input().split())]
 
-if m > n:
+if m > n:#무조건 n이 m보다 크거나 같게 만든다
     n,m = m,n
-    tmp = N[::]
-    N = M[::]
-    M = tmp[::]
-N.sort()
-M.sort()
-n,m = n+1,m+1
-dp = [[INF] * m for _ in range(n)]
-print(dfs(1,1))
+    tmp = arrn[::]
+    arrn = arrm[::]
+    arrm = tmp[::]#깊은 복사
+arrn.sort() #역순으로 정렬한다.
+arrm.sort()
+
+dp = [[-1]*(n)for _ in range(m)]
+#여기서 해야 되는 것이 m이 무조건 작음
+#n - m 
+def dfs(depth, index):#해당 depth에 index를 골랐을 때 최대 값
+    if depth == m:
+        return 0
+    if index >= n:return 10**9 + 1
+    if dp[depth][index]!=-1:return dp[depth][index]
+
+    for j in range(index, n-(m-depth)+1):#depth랑 j랑 차이 계산 후 더해주기 n - (m - (depth+1))
+        ret = dfs(depth+1,j+1) + abs(arrm[depth] - arrn[j])
+        if dp[depth][index] == -1:dp[depth][index] = ret
+        else : dp[depth][index] = min(dp[depth][index],ret)
+    return dp[depth][index]
+print(dfs(0,0))
