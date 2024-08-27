c0ac58d0d2179e01b47e81d5e94e5c928ae262e7
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-27 22:49:55 +0900

    Title: 최소 차이로 묶기, Time: 1099ms, Memory: 4MB, Status: Time Limit Exceed - CodeTree

--- 240827/최소 차이로 묶기/bundling-with-minimum-difference.cpp
+++ 240827/최소 차이로 묶기/bundling-with-minimum-difference.cpp
@@ -1,49 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n,m;
-int arrn[1002];
-int arrm[1002];
-int tmp[1002];
-int main() {
-    cin.tie(0) -> sync_with_stdio(0);
+int n, m;
+int dp[1005][1005];
+int v[2][1005];
+const int INF = 0x3f3f3f3f;
+int dfs(int x, int y){
+    if(y == n)return 0;
+    if (x == m)return INF;
+    if (dp[x][y] != INF)return dp[x][y];
+
+    dp[x][y] = min(dfs(x+1,y), dfs(x+1,y+1)+abs(v[1][x] - v[0][y]));
+    return dp[x][y];
+}
+int main(){
+    cin.tie(0) -> ios::sync_with_stdio(0);
+    memset(dp, 0x3f, sizeof dp);
+
     cin >> n >> m;
-    for(int i=0;i<n;i++){
-        cin >> arrn[i];
-        tmp[i] = arrn[i];
-    }
-    for(int j=0;j<m;j++){
-        cin >> arrm[j];
-    }
-    if(m > n){
-        for(int i=0;i<m;i++){
-            arrn[i] = arrm[i];
-        }
-        for(int i=0;i<n;i++){
-            arrm[i] = tmp[i];
-        }
+    for(int i=1;i<=n;i++)cin >> v[0][i];
+    for(int i=1;i<=m;i++)cin >> v[1][i];
+
+    sort(v[0]+1,v[0]+n+1);
+    sort(v[1]+1,v[1]+m+1);
+
+    if(m < n){//m이 무조건 큰 것
         swap(n,m);
+        swap(v[0],v[1]);
     }
-    sort(arrn,arrn+n);
-    sort(arrm,arrm+m);
-    
-    int ret{};
-
-     int start = 0;
-     for(int i=0;i<m;i++){
-         int end = n - (m - i - 1);//[start, end]일 때 arr[i]와 가장 가까운 값
-
-         int gap = 1e6 ;
-         int indx = -1;
-         for(int j= start; j < end;j++){
-             if(abs(arrn[j] - arrm[i]) < gap){
-                 gap = abs(arrn[j] - arrm[i]);
-                 indx = j;
-             }
-         }
-         start = indx+1;
-         ret += gap;
-     }
-
-     cout << ret;   
+    n++,m++;
+    cout << dfs(1,1);
     return 0;
 }
