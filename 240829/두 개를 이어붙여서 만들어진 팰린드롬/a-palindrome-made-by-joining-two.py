c0b9046fc309551644fa510435f23bc914cc1d7d
Striver <96542456+T3Tm@users.noreply.github.com>
2024-08-29 00:35:25 +0900

    Title: 두 개를 이어붙여서 만들어진 팰린드롬, Time: 8037ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240829/두 개를 이어붙여서 만들어진 팰린드롬/a-palindrome-made-by-joining-two.py
+++ 240829/두 개를 이어붙여서 만들어진 팰린드롬/a-palindrome-made-by-joining-two.py
@@ -4,11 +4,12 @@ arr = input().split()
 result = 0
 for t in range(n-1):
     for m in range(t+1,n):
-        word = '#'+ '#'.join(arr[t] + arr[m])+'#'
-        words = '#'+'#'.join(arr[m] + arr[t]) + '#'
+        if len(arr[t]) < len(arr[m]):
+            word = '#'+ '#'.join(arr[t] + arr[m])+'#'
+        else:
+            word = '#'+ '#'.join(arr[m] + arr[t])+'#'
         
         A = [0] * len(word)
-        AA = [0] * len(words)
         #마나커 알고리즘
         r = q = -1
         for i in range(1, len(word)):
@@ -23,16 +24,4 @@ for t in range(n-1):
         
         if 2*A[len(word)//2]+1 == len(word):#이 곳의 값이 반지름인지 확인
             result = max(result, len(word)//2)
-        r = q = -1
-        for i in range(1, len(words)):
-            if r >= i:
-                ii = 2 * q - i
-                AA[i] = min(r-i, AA[ii])
-            while i - AA[i] - 1 >=0 and i + AA[i] + 1 < len(words) and \
-                words[i - AA[i] - 1] == words[i + AA[i] + 1]:
-                    AA[i]+=1
-            if i + AA[i] > r:
-                r, q = i + AA[i], i
-        if 2*AA[len(words)//2]+1 == len(words):#이 곳의 값이 반지름인지 확인
-            result = max(result, len(words)//2)
 print(result)
