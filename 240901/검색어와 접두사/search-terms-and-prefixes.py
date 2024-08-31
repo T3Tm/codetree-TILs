f64a29f941ad6e7437970e0aa5a7a3abdd0ab79d
Striver <96542456+T3Tm@users.noreply.github.com>
2024-09-01 01:14:42 +0900

    Title: 검색어와 접두사, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240901/검색어와 접두사/search-terms-and-prefixes.py
@@ -0,0 +1,38 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+trie = {}
+
+
+def insert(string):
+    cur = trie
+    for v in string:
+        if cur.get(v, 0) == 0:
+            cur[v] = {}
+        cur = cur[v]
+    cur[0] = 1#끝을 알리는 것
+def find(string, result):
+    cur = trie
+    for idx in range(len(string)):
+        if cur.get(string[idx], 0) == 0:break
+        cur = cur[string[idx]]
+        result[idx] = cur[1]
+def preProcessing(cur):
+    #해당 cur에는 단어가 총 몇개 있는지 확인하기
+    cur[1] = cur.get(0, 0)
+    for key in cur.keys():
+        if key == 1 or key == 0:continue
+        cur[1] = cur[1] + preProcessing(cur[key])
+    return cur[1]
+for word in input().split():
+    insert(word)
+
+for key in trie.keys():
+    preProcessing(trie[key])
+
+target = input().rstrip()
+result = [0] * m
+find(target, result)
+print(*result)
