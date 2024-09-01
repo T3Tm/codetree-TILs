a54d09191f7576d32ebb52f60f1b5abb4f07038a
Striver <96542456+T3Tm@users.noreply.github.com>
2024-09-02 00:02:29 +0900

    Title: 트리 파악하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240902/트리 파악하기/figure-out-the-tree.py
@@ -0,0 +1,28 @@
+import sys
+sys.setrecursionlimit(1001)
+input = sys.stdin.readline
+
+n = int(input())
+
+trie = {}
+
+def insert(List):
+    cur = trie
+    for word in List:
+        if cur.get(word, 0) == 0:
+            cur[word] = {}
+        cur = cur[word]
+    cur[0] = 1
+
+def prints(cur, depth):
+    for key in sorted(cur.keys()):
+        if key == 0:continue
+        print('--' * depth + key)
+        prints(cur[key], depth + 1)
+
+
+for _ in range(n):
+    cnt, *List = input().split()
+    insert(List)
+
+prints(trie, 0)
