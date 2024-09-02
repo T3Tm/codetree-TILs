8c8e1d6eb06534501c0a421bf274db47b410a959
Striver <96542456+T3Tm@users.noreply.github.com>
2024-09-02 23:22:39 +0900

    Title: 맞닿지 않은 사각형 선택, Time: 437ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240902/맞닿지 않은 사각형 선택/Select-non-contact-rectangles.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+dp = [[0] * (n+1) for _ in range(n)]
+
+
+arr = [[0] + [*map(int,input().split())]for _ in range(2)]
+
+
+
+for j in range(2):
+    dp[j][1] = dp[j^1][0] + arr[j][1]
+
+for i in range(2,n+1):
+    for j in range(2):
+        dp[j][i] = max(dp[j^1][i-1], dp[j^1][i-2], dp[j][i-2]) + arr[j][i]
+
+print(max(dp[0][-1], dp[1][-1]))
