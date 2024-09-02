959814c852b466d89a2dca169e90640a57d5415b
Striver <96542456+T3Tm@users.noreply.github.com>
2024-09-02 23:13:19 +0900

    Title: 연속된 수의 합의 최대, Time: 146ms, Memory: 32MB, Status: Passed - CodeTree

--- 240902/연속된 수의 합의 최대/maximum-sum-of-consecutive-numbers.py
+++ 240902/연속된 수의 합의 최대/maximum-sum-of-consecutive-numbers.py
@@ -5,19 +5,19 @@
 
 #나를 더해서 cnt +1 개를 만들거나
 #나 부터 시작해서 1개로 들어가도 됨.
-
-INF = 10**9
 n, k =map(int ,input().split())
-arr = [*map(int, input().split())]
+arr = [0] + [*map(int, input().split())]
 
 prefix = [0]*(n+1)
 
-for i in range(1, k+1):
-    prefix[i] = prefix[i-1] + arr[i-1]
+for i in range(1, n+1):
+    prefix[i] = prefix[i-1] + arr[i]
 
+
+dp = [0] * (n+1)
 #k개는 연속적으로 선택했어야 하니까
+dp[k] = prefix[k]
+for i in range(k+1, n+1):#새로운 k개를 달성하는 것이 더 좋음
+    dp[i] = max(dp[i-1] + arr[i], prefix[i] - prefix[i-k])
 
-for i in range(k+1, n+1):
-    prefix[i] = max(prefix[i-1] + arr[i-1], prefix[i-1] + arr[i-1] - prefix[i-k])
-
-print(max(prefix[k:]))
+print(max(dp[k:]))
