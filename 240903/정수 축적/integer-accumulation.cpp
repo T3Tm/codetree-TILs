b4df9ba2389c186d2f7b10acb9491032b11ceacd
Striver <96542456+T3Tm@users.noreply.github.com>
2024-09-03 17:30:25 +0900

    Title: 정수 축적, Time: 67ms, Memory: 38MB, Status: Wrong Answer - CodeTree

---
+++ 240903/정수 축적/integer-accumulation.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int dp[10002][1000];
+int n,m;
+int arr[10002];
+int dfs(int x,int y){
+    if(x >=n)return -1;
+    else if(x == n-1){
+        if(y == 0)return 0;
+        return -1;
+    }
+    if(y == m)return dfs(x+m-1,0);
+    if(dp[x][y] !=-1)return dp[x][y];
+    dp[x][y] = 0;
+    int ret1 = dfs(x+1,y+1);
+    int ret2 = dfs(x+1,y);
+    if(ret1!=-1)dp[x][y] = ret1 + arr[x];
+    if(ret2 !=-1)dp[x][y] = max(dp[x][y], ret2);
+    return dp[x][y];
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m;
+    for(int i=0;i<n;i++)cin >> arr[i];
+    memset(dp, -1, sizeof dp);
+    cout << dfs(0,0);
+    return 0;
+}
