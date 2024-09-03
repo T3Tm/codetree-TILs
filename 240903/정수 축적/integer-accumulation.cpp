d0c3559a8c63b008045c9ae3595126046031e7cf
Striver <96542456+T3Tm@users.noreply.github.com>
2024-09-03 17:58:25 +0900

    Title: 정수 축적, Time: 1075ms, Memory: 38MB, Status: Time Limit Exceed - CodeTree

--- 240903/정수 축적/integer-accumulation.cpp
+++ 240903/정수 축적/integer-accumulation.cpp
@@ -5,16 +5,15 @@ int dp[10002][1000];
 int n,m;
 int arr[10002];
 int dfs(int x,int y){
-    if(x >=n)return -1;
-    else if(x == n-1){
+    if(x > n)return -1;
+    else if(x == n){
         if(y == 0)return 0;
         return -1;
     }
-    if(y == m)return dfs(x+m-1,0);
+    if(y == m)return dfs(x+m,0);
     if(dp[x][y] !=-1)return dp[x][y];
-    dp[x][y] = 0;
     int ret1 = dfs(x+1,y+1);
-    int ret2 = dfs(x+1,y);
+    int ret2 = dfs(x+1,max(y-1,0));
     if(ret1!=-1)dp[x][y] = ret1 + arr[x];
     if(ret2 !=-1)dp[x][y] = max(dp[x][y], ret2);
     return dp[x][y];
